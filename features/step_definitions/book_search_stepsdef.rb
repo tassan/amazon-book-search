77a35a1ef665344e1c5e63e056ab9a13b33448ae
Flávio Tassan <tassanf@gmail.com>
2020-04-02 12:54:09 -0300

    Criação dos primeiros steps

---
+++ features/step_definitions/book_search_stepsdef.rb
@@ -0,0 +1,11 @@
+Given("I am on Amazon search page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I search for {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("the page title should start with {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
